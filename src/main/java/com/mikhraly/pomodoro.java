b949a2edb773afeddc7a13a79f643d75f5230dc2
Mikhraly <mikhraly@gmail.com>
2022-06-12 15:29:18 +0300

    change java from 18 to 17 chance 2

--- src/main/java/com/mikhraly/pomodoro.java
+++ src/main/java/com/mikhraly/pomodoro.java
@@ -2,6 +2,6 @@ package com.mikhraly;
 
 public class pomodoro {
     public static void main(String[] args) {
-        System.out.println("Hello, pomodoro");
+        System.out.println("Hello pomodoro");
     }
 }
