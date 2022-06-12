b34a7b4725bfba7b3b7ca72e2281a0bf3f0cdb25
Mikhraly <mikhraly@gmail.com>
2022-06-12 15:20:13 +0300

    change java from 18 to 17

--- src/main/java/com/mikhraly/pomodoro.java
+++ src/main/java/com/mikhraly/pomodoro.java
@@ -2,6 +2,6 @@ package com.mikhraly;
 
 public class pomodoro {
     public static void main(String[] args) {
-        System.out.println("Hello, motherfucker");
+        System.out.println("Hello, pomodoro");
     }
 }
